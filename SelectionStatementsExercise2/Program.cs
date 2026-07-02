406d98e51cd316f7e7f51281f976fecef8cbb61a
Ashley Doering <Ashley.doering@outlook.com>
2026-07-02 17:06:31 -0400

    Made a prompt asking people what their favorite subject is with a switch response

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+        
+        public static void FavSubject()
+        
+        {
+            Console.WriteLine("What is your favorite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "science":
+                    Console.WriteLine("I love science too");
+                    break;
+                case "music":
+                    Console.WriteLine("Music is an awesome subject");
+                    break;
+                case "math":
+                    Console.WriteLine("Math is great,but super difficult for me");
+                    break;
+                case "history":
+                    Console.WriteLine("History is one of the best subjects");
+                    break;
+                default:
+                    Console.WriteLine("I like that too");
+                    break;
+                
+            }
+            
         }
     }
 }
